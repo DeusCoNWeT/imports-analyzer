d5819b651659e65daf2fa440b2c5b9dbc5f91830
Guillermo Carrera Trasobares <--global>
2017-03-09 15:12:52 +0100

    Working, still missing total imports

--- final.py
+++ final.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from bs4 import BeautifulSoup
 
+
+
 if len(sys.argv) < 2:
     cwd = os.path.dirname(os.path.abspath(__file__)) + '/index.html'
     print  ("Error!! Introducir directorio a analizar como primer argumento")
@@ -15,49 +17,61 @@ else:
 
 def removerPuntoPunto(url):
     if url.startswith("./"):
-        urlacortada = url.replace(url[:1], "")
+        urlacortada = url[1:]
+        return urlacortada
+    if url.startswith("../../"):
+        urlacortada = url[5:]
         return urlacortada
     if url.startswith("../"):
-        urlacortada = url.replace(url[:2], "")
+        urlacortada = url[2:]
         return urlacortada
-    else:
-        if url.startswith("../../"):
-            urlacortada = url.replace(url[:6], "")
-            return urlacortada
     return  url
 
-
+def removeDups(inputfile, outputfile):
+    count=1
+    lines=open(inputfile, 'r').readlines()
+    lines_set = set(lines)
+    out=open(outputfile, 'w')
+    for line in lines_set:
+        out.write(str(count)+': '+line)
+        count=count+1
 
 def funcionRecursiva(dirrel):
     dirajust = removerPuntoPunto(dirrel)
     fileopen= open (dirajust)
     soup2= BeautifulSoup(fileopen,"html.parser")
-    print('PADRE:'+dirajust)
-    for link in soup2.find_all('link'):
-        
-        if(link.get('href'))=='':
-            break
-        else:
-           auxi = str(link.get('href'))
-           if auxi.startswith("../"):
-               auxi2=str(Path(dirajust).parent.parent)
-               auxi= auxi2 + removerPuntoPunto(auxi)
-               funcionRecursiva(auxi)
-           elif auxi.startswith("/"):
-                funcionRecursiva(auxi)
-           else:
-               print("AUXI VALE:" + auxi + '\n')
-               auxi =  str(Path(dirajust).parent) + '/' + auxi 
-               funcionRecursiva(auxi)
+    with open("importsTotales.txt", "a") as myfile:
+        for link in soup2.find_all(rel="import"):
+            myfile.write(dirajust+'\n')
+            print('Import number:' + dirajust)
+            if(link.get('href'))=='':
+                break
+            else:
+                auxi = str(link.get('href'))
+                if(auxi.startswith("./")):
+                    #print("Empieza con ./:   "+auxi + '\n')
+                    #print("El resultado de removerPuntoPunto es: " + removerPuntoPunto(auxi))
+                    auxi=str(Path(dirajust).parent) + removerPuntoPunto(auxi)
+                    funcionRecursiva(auxi)
+                if(auxi.startswith("../../")):
+                    auxi2=str(Path(dirajust).parent.parent.parent)
+                    auxi= auxi2 + removerPuntoPunto(auxi)
+                    #print("Empieza con ../../:  "+auxi+'\n')
+                if auxi.startswith("../"):
+                    auxi2=str(Path(dirajust).parent.parent)
+                    auxi= auxi2 + removerPuntoPunto(auxi)
+                    #print("Empieza con ../:  "+auxi+'\n')
+                    funcionRecursiva(auxi)
+                elif auxi.startswith("/"):
+                    #print("Empieza con /:   "+auxi + '\n')
+                    funcionRecursiva(auxi)
+                else:
+                    #print("Import a tratar:  " + auxi + '\n')
+                    auxi =  str(Path(dirajust).parent) + '/' + auxi 
+                    funcionRecursiva(auxi)
        
-           
-
-
-
-
-
-
-funcionRecursiva('/home/gcarrerat/proyectos/pythonparser/note-app.html')
+funcionRecursiva('/home/gcarrerat/proyectos/pythonparser/index.html')
+removeDups('importsTotales.txt', 'importsCribados.txt')
 
 
 

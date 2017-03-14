2222b77f7a7ec1bfd381f2f6f7778812ecd32732
Guillermo Carrera Trasobares <--global>
2017-03-14 13:24:36 +0100

    Remove FunPunto

--- final.py
+++ final.py
@@ -1,4 +1,5 @@
 import os
+import json
 import sys
 import fnmatch
 from pathlib import Path
@@ -8,58 +9,63 @@ from bs4 import BeautifulSoup
 
 
 if len(sys.argv) < 2:
-    cwd = os.path.dirname(os.path.abspath(__file__)) + '/index.html'
+    cwd = os.getcwd() + '/index.html'
     print  ("Error!! Introducir directorio a analizar como primer argumento")
     print (" Se utilizara el directorio actual: " + cwd + "\n\n")
 else:
     cwd = sys.argv[1]
 
 
-def removerPuntoPunto(url):
-    if url.startswith("./"):
-        urlacortada = url[1:]
-        return urlacortada
-    if url.startswith("../../"):
-        urlacortada = url[5:]
-        return urlacortada
-    if url.startswith("../"):
-        urlacortada = url[2:]
-        return urlacortada
-    return  url
-
 def removeDups(inputfile, outputfile):
     count=1
     lines=open(inputfile, 'r').readlines()
     lines_set = set(lines)
     out=open(outputfile, 'w')
-    for line in lines_set:
-        out.write(str(count)+': '+line)
-        count=count+1
+    with open("jsonFinal.json","w") as outfile:
+        for line in lines_set:
+            out.write(str(count)+': '+line)
+            #Eliminate the last \n while parsing the json
+            json.dump([{'directorio:':line[:-1], 'numero:':count}],outfile,indent=4)
+            count=count+1
+
+def listAllFiles(dirrel):
+    count=1
+    for subdir, dirs, files in os.walk(dirrel):
+        for file in files:
+            print(str(count) + ': '+ os.path.join(subdir, file))
+            count=count+1
+
+def listAllImports(dirrel):
+    return
+
+def txtToJsonDump(inFile):
+    return
+
 
 def funcionRecursiva(dirrel):
-    dirajust = removerPuntoPunto(dirrel)
-    fileopen= open (dirajust)
-    soup2= BeautifulSoup(fileopen,"html.parser")
+    dirajust = dirrel
+    fileopen = open(dirajust)
+    soup2 = BeautifulSoup(fileopen, "html.parser")
     with open("importsTotales.txt", "a") as myfile:
         for link in soup2.find_all(rel="import"):
             myfile.write(dirajust+'\n')
             print('Import number:' + dirajust)
-            if(link.get('href'))=='':
+            if link.get('href') == '':
                 break
             else:
                 auxi = str(link.get('href'))
-                if(auxi.startswith("./")):
+                if auxi.startswith("./"):
                     #print("Empieza con ./:   "+auxi + '\n')
                     #print("El resultado de removerPuntoPunto es: " + removerPuntoPunto(auxi))
-                    auxi=str(Path(dirajust).parent) + removerPuntoPunto(auxi)
+                    auxi = str(Path(dirajust).parent) + auxi[1:]
                     funcionRecursiva(auxi)
-                if(auxi.startswith("../../")):
-                    auxi2=str(Path(dirajust).parent.parent.parent)
-                    auxi= auxi2 + removerPuntoPunto(auxi)
+                if auxi.startswith("../../"):
+                    auxi2 = str(Path(dirajust).parent.parent.parent)
+                    auxi = auxi2 + auxi[5:]
                     #print("Empieza con ../../:  "+auxi+'\n')
                 if auxi.startswith("../"):
-                    auxi2=str(Path(dirajust).parent.parent)
-                    auxi= auxi2 + removerPuntoPunto(auxi)
+                    auxi2 = str(Path(dirajust).parent.parent)
+                    auxi = auxi2 + auxi[2:]
                     #print("Empieza con ../:  "+auxi+'\n')
                     funcionRecursiva(auxi)
                 elif auxi.startswith("/"):
@@ -67,10 +73,11 @@ def funcionRecursiva(dirrel):
                     funcionRecursiva(auxi)
                 else:
                     #print("Import a tratar:  " + auxi + '\n')
-                    auxi =  str(Path(dirajust).parent) + '/' + auxi 
+                    auxi = str(Path(dirajust).parent) + '/' + auxi
                     funcionRecursiva(auxi)
-       
+
 funcionRecursiva('/home/gcarrerat/proyectos/pythonparser/index.html')
+#listAllFiles('/home/gcarrerat/proyectos/pythonparser/')
 removeDups('importsTotales.txt', 'importsCribados.txt')
 
 

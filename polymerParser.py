4802fbae27f7622b5381272835df9f19268119a6
Miguel Ortega Moreno <mortega@conwet.com>
2017-03-30 10:01:33 +0200

    Fixed bugs

--- polymerParser.py
+++ polymerParser.py
@@ -1,88 +1,113 @@
 # Guillermo Carrera Trasobares 2017
 
-import os, sys, fnmatch, argparse, json
-from pathlib import Path
+import os
+import sys
+import argparse
+import json
+import re
 from bs4 import BeautifulSoup
+from mixpanel import Mixpanel
 
-parser= argparse.ArgumentParser()
-parser.add_argument('-u','--url',help="Introduce the URL you want to start parsing")
-parser.add_argument('-o','--output',help="Introduce the desired output: txt, json.....")
-parser.add_argument('-s','--sorted',help="Sorts the output",action='store_true')
-parser.add_argument('-l','--listFiles',help="Lists all files inside the selected directory and sub-directories",action='store_true')
+DEFAULT_FILE = "totalImports.json"
+DEFAULT_JSON_FILE = "imports.json"
+DEFAULT_SORT_FILE = "sortedImports.txt"
+
+MIXPANEL_TOKEN = "ec0790cae6fb383f1cf7cab42a72c803"
+MIXPANEL = Mixpanel(MIXPANEL_TOKEN)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-u', '--url', help="Introduce the URL you want to start parsing")
+parser.add_argument(
+    '-f', '--file', help="Introduce the URL you want to start parsing")
+parser.add_argument(
+    '-o', '--output', help="Introduce the desired output: txt, json.....")
+parser.add_argument(
+    '-s', '--sorted', help="Sorts the output", action='store_true')
+parser.add_argument('-l', '--listFiles',
+                    help="Lists all files inside the selected directory and sub-directories",
+                    action='store_true')
+
 args = parser.parse_args()
 
-#if len(sys.argv) < 2:
-#   cwd = os.getcwd() + '/index.html'
-#   print  ("Error!! Introducir directorio a analizar como primer argumento")
-#  print (" Se utilizara el directorio actual: " + cwd + "\n\n")
-#else:
-#    cwd = sys.argv[1]
-
+def sendToMixpanel(report):
+  event = '%s (%s)' %(report['file_name'], report['version'])
+  MIXPANEL.track(report['file_name'], event, report)
 def removeDups(inputfile, outputfile):
-    lines=open(inputfile, 'r').readlines()
+    lines = open(inputfile, 'r').readlines()
     lines_set = set(lines)
-    out=open(outputfile, 'w')
+    out = open(outputfile, 'w')
     for line in lines_set:
         out.write(line)
-        
+
+
 def listAllFiles(dirrel):
-    count=1
-    for subdir, dirs, files in os.walk(dirrel):
-        for file in files:
-            print(str(count) + ': '+ os.path.join(subdir, file))
-            count=count+1
+    count = 1
+    for subdir, _, files in os.walk(dirrel):
+        for _file in files:
+            print str(count) + ': ' + os.path.join(subdir, _file)
+            count = count + 1
+
 
 def createJson(inputfile):
-    count=1
-    lines=open(inputfile, 'r').readlines()
-    with open("imports.json",'w') as outfile:
-        outfile.write("[")
-        for line in lines[:-1]:
-            #Eliminate the last \n while parsing the json
-            json.dump({'url:':line[:-1], 'number:':count},outfile, indent=3)
-            outfile.write(",")
-            count=count+1
-        for line in lines[-1:]:
-            json.dump({'url:':line[:-1], 'number:':count},outfile, indent=3)
-            outfile.write("]")
+    fd = open(inputfile, 'r')
+    lines = fd.readlines()
+    fd.close()
+    lines_without_repeted = list(set(lines))
+    lines_without_repeted = [line.replace(
+        '\n', '') for line in lines_without_repeted]
+    with open(DEFAULT_JSON_FILE, 'w') as outputfile:
+        json.dump(sorted(lines_without_repeted), outputfile, indent=2)
+    return lines_without_repeted, lines
+
 
 def funcionRecursiva(dirrel):
-    dirajust = dirrel
-    fileopen = open(dirajust)
+    fileopen = open(dirrel)
     soup2 = BeautifulSoup(fileopen, "html.parser")
-    with open("totalImports.txt", "a") as myfile:
+    base_dir = os.path.dirname(dirrel)
+    with open(DEFAULT_FILE, "a") as myfile:
         for link in soup2.find_all(rel="import"):
             #print('Import number:' + dirajust)
             if link.get('href') == '':
                 break
             else:
                 auxi = str(link.get('href'))
-                if auxi.startswith("./"):
-                    #print("Empieza con ./:   "+auxi + '\n')
-                    #print("El resultado de removerPuntoPunto es: " + removerPuntoPunto(auxi))
-                    auxi = str(Path(dirajust).parent) + auxi[1:]
-                    myfile.write(auxi+'\n')
-                    funcionRecursiva(auxi)
-                if auxi.startswith("../../"):
-                    auxi2 = str(Path(dirajust).parent.parent.parent)
-                    auxi = auxi2 + auxi[5:]
-                    #print("Empieza con ../../:  "+auxi+'\n')
-                    myfile.write(auxi+'\n')
-                    funcionRecursiva(auxi)
-                if auxi.startswith("../"):
-                    auxi2 = str(Path(dirajust).parent.parent)
-                    auxi = auxi2 + auxi[2:]
-                    #print("Empieza con ../:  "+auxi+'\n')
-                    myfile.write(auxi+'\n')
-                    funcionRecursiva(auxi)
-                elif auxi.startswith("/"):
-                    #print("Empieza con /:   "+auxi + '\n')
-                    funcionRecursiva(auxi)
-                else:
-                    #print("Import a tratar:  " + auxi + '\n')
-                    auxi = str(Path(dirajust).parent) + '/' + auxi
-                    myfile.write(auxi+'\n')
-                    funcionRecursiva(auxi)
+                url = os.path.abspath(base_dir + '/' + auxi)
+                myfile.write(url + '\n')
+                funcionRecursiva(url)
+
+def generateReport(file_url):
+    f = open(DEFAULT_FILE, 'w')
+    f.close()
+    funcionRecursiva(file_url)
+    if args.output and args.sorted:
+        if args.output == "json":
+            removeDups(DEFAULT_FILE, DEFAULT_SORT_FILE)
+            total_lines, lines = createJson(DEFAULT_SORT_FILE)
+            #match_regex = re.search(r'\/[^-]*-[^-]*-([^\/]*)\/', file_url)
+            #version = match_regex.group(1)
+            version = "structural"
+            print "%s - %d imports (totales %d)" % (file_url, len(total_lines), len(lines))
+
+            return {"total_lines":len(lines),
+                    "file_name":os.path.basename(file_url), "single_imports":len(total_lines),
+                    "version":version}
+        else:
+            print "generating default .txt output"
+            removeDups(DEFAULT_FILE, DEFAULT_SORT_FILE)
+    else:
+        if args.output:
+            if args.output == "json":
+                total_lines, lines = createJson(DEFAULT_FILE)
+                #match_regex = re.search(r'\/[^-]*-[^-]*-([^\/]*)\/', file_url)
+                #version = match_regex.group(1)
+                version = "structural"
+                print "%s - %d imports (totales %d)" % (file_url, len(total_lines), len(lines))
+                return {"total_lines":len(lines),
+                        "file_name":os.path.basename(file_url), "single_imports":len(total_lines),
+                        "version":version}
+    if args.sorted:
+        removeDups(DEFAULT_FILE, DEFAULT_SORT_FILE)
 
 
 if args.listFiles:
@@ -90,32 +115,24 @@ if args.listFiles:
         listAllFiles(args.url)
         sys.exit()
     else:
-        print ("No directory selected, using current one\n\n")
+        print "No directory selected, using current one\n\n"
         listAllFiles(os.getcwd())
         sys.exit()
 
+
 if args.url:
-    print (args.url)
-    funcionRecursiva(args.url)
-    if args.output and args.sorted:
-        if args.output == "json":
-            removeDups('totalImports.txt', 'sortedImports.txt')
-            createJson('sortedImports.txt')
-            sys.exit()
-        else:
-            print ("generating default .txt output")
-            removeDups('totalImports.txt', 'sortedImports.txt')
-            sys.exit()
-    else:
-        if args.output:
-            if args.output == "json":
-                createJson('totalImports.txt')
-                sys.exit()
-    if args.sorted:
-        removeDups('totalImports.txt', 'sortedImports.txt')
-                
-            
-                
+    f = open(DEFAULT_FILE, 'w')
+    f.close()
+    report = generateReport(args.url)
+    output_file = open(DEFAULT_JSON_FILE, 'w')
+    json.dump(report, output_file, indent=2)
+    #sendToMixpanel(report)
 
+elif args.file:
+    components = open(args.file).readlines()
+    report_list = [generateReport(file_url.replace('\n', '')) for file_url in components]
+    output_file = open(DEFAULT_JSON_FILE, 'w')
+    json.dump(report_list, output_file, indent=2)
 
-
+    ## SEND TO MIXPANEL
+    [ sendToMixpanel(report) for report in report_list ]
